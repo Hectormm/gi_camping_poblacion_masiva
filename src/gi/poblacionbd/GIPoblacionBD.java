d8918f191f15a6238d9fa406bbfd420f1a73b121
mfg34 <marinafornes34@gmail.com>
2014-04-14 18:20:14 +0200

    añade dni nombre apellido direccion telefono email
    Copia cada cliente en una fila al .cvs pero llega un momento casi en el
    100.000 que da un error, creo q será algo de las posiciones de los
    arrays, de la cantidad de alguno.

--- src/gi/poblacionbd/GIPoblacionBD.java
+++ src/gi/poblacionbd/GIPoblacionBD.java
@@ -25,6 +25,10 @@ public class GIPoblacionBD {
 
     private static ArrayList<String> nombres;
     private static ArrayList<String> apellidos;
+    private static ArrayList<String> apellidos2;
+    private static ArrayList<String> dnis;
+    private static ArrayList<String> telefonos;
+    private static ArrayList<String> calles;
     /**
      * @param args the command line arguments
      * @throws java.io.UnsupportedEncodingException
@@ -34,6 +38,13 @@ public class GIPoblacionBD {
         throws UnsupportedEncodingException, FileNotFoundException, IOException, ParseException {
 
         nombres = new ArrayList<>();
+        apellidos = new ArrayList<>();
+        apellidos2 = new ArrayList<>();
+        dnis = new ArrayList<>();
+        calles = new ArrayList<>();
+        telefonos = new ArrayList<>();
+        
+        boolean nom=true;
         
          //construimos una instancia de la clase
         LeerEscribirFicheroCsv le = new LeerEscribirFicheroCsv(); 
@@ -41,29 +52,68 @@ public class GIPoblacionBD {
         ArrayList<String[]>f = le.LeerFicheroCsv("nombresHombresEsp.csv");
         
         ArrayList<String[]>a = le.LeerFicheroCsv("apellidosEspanyoles.csv");
+
+        ArrayList<String[]>d = le.LeerFicheroCsv("dni.csv");
         
-        double totalInserciones = generar(f, 586450/2);
+         int numerodnis = d.size();
+        String numero;
+        String letra;
         
-        double totalApellidos = generar(a, 655000);
+        for(int i = 0; i < numerodnis; i++){
+         
+            numero = d.get(i)[0];
+            letra = d.get(i)[1];
+            String dni = numero+letra;
+            dnis.add(dni);
+        }
         
-        a = le.LeerFicheroCsv("apellidosExtranjeros.csv");
-       totalApellidos += generar(a, 594000);
+        ArrayList<String[]>c = le.LeerFicheroCsv("calles.csv");
+        c = le.LeerFicheroCsv("callesExt.csv");
+        
+          int numerocalles = c.size();
+        String calle;
+        
+        for(int i = 0; i < numerocalles; i++){
+         
+            calle = c.get(i)[0];
+            calles.add(calle);
+        }
+        
+        ArrayList<String[]>t = le.LeerFicheroCsv("telefonos.csv");
+        
+         int numerotelefonos = t.size();
+         String telefono;
+        
+        for(int i = 0; i < numerotelefonos; i++){
+         
+            telefono = t.get(i)[0];
+            telefonos.add(telefono);
+        }
+        
+        double totalInserciones = generar(f, 586450/2, nom);
         
         f = le.LeerFicheroCsv("nombresMujeresEsp.csv");
-       totalInserciones += generar(f, 605168/2);
+       totalInserciones += generar(f, 605168/2, nom);
         
         f = le.LeerFicheroCsv("nombresMujeresExt.csv");
-        totalInserciones += generar(f, 35095);
+        totalInserciones += generar(f, 35095, nom);
         
         f = le.LeerFicheroCsv("nombresHombresExt.csv");
-        totalInserciones += generar(f, 32833);
+        totalInserciones += generar(f, 32833, nom);
+        
+        nom=false;
+        
+        double totalApellidos = generar(a, 655000, nom);
+        
+        a = le.LeerFicheroCsv("apellidosExtranjeros.csv");
+       totalApellidos += generar(a, 594000, nom);
         
         insertar(totalInserciones, totalApellidos);
         
     }
     
     
-    public static double generar(ArrayList<String[]> f, double numeroMagico) throws FileNotFoundException, ParseException, IOException{
+    public static double generar(ArrayList<String[]> f, double numeroMagico, boolean nom) throws FileNotFoundException, ParseException, IOException{
             
         int totalNombres = f.size();
         double inserciones, total = 0;
@@ -79,11 +129,22 @@ public class GIPoblacionBD {
             inserciones = Math.ceil(porcentaje*numeroMagico);
             
             nombre = f.get(i)[0];
+            apellido = f.get(i)[0];
             
-            for(int j=0; j<inserciones; j++)
+            if(nom==true){
+               for(int j=0; j<inserciones; j++)
                 nombres.add(nombre);
             
+            total += inserciones; 
+            }
+            else{
+                for(int j=0; j<inserciones; j++)
+                apellidos.add(apellido);
+            
             total += inserciones;
+            }
+            
+            
         }
         
         System.out.print("Total de inerciones: ");
@@ -96,17 +157,28 @@ public class GIPoblacionBD {
         int randomNum;
         double cant;
         double totalInserciones2 = totalInserciones;
+        int k=0;
+
         
         try (OutputStream fout = new FileOutputStream("salida.csv"); 
                 OutputStreamWriter out = new OutputStreamWriter(fout, "UTF8")) {
             
             for(int i=0; i< totalInserciones; i++){
-                randomNum = (int)(Math.random()*totalInserciones2); 
-
-                out.write(nombres.get(randomNum)+";Martinez Rodriguez\n");
+                randomNum = (int)(Math.random()*totalInserciones2);
+               
+               String nomFinal = nombres.get(randomNum);
+               char l0=nomFinal.charAt(0);
+               
+               String apellidoFinal = apellidos.get(randomNum);
+               char l1=apellidoFinal.charAt(0);
+                
+                out.write(dnis.get(k)+";"+nomFinal+";"+apellidoFinal+";"+calles.get(k)+";"+telefonos.get(k)+";"+l0+l1+"@gmail.com"+"\n");
                 
                 nombres.remove(randomNum);
+                apellidos.remove(randomNum);
+                k++;
                 totalInserciones2--;
+ 
             }
         }
     }
